43216f6f95d0994e3f5242218c25cddae75de918
Dakota Cain <cain_dakota@ou.edu>
2020-01-30 13:16:36 -0600

    Finished participation WPF1

---
+++ Participation/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            EntryForm ef = new EntryForm();
+            ef.Name = txtName.Text;
+            ef.Address = txtAddress.Text;
+            ef.ZipCode = Convert.ToInt32(txtZip.Text);
+
+            lstUsers.Items.Add(ef);
+            
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
